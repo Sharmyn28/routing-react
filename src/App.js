e4c6ad68bc53e69ddcdb6e3d96d3b15419751d71
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-10-06 13:07:50 -0500

    till teachers working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TeacherList from './teachers';
 
 class Home extends Component {
 	render() {
 		return (
 			<div className="main-content home">
-				<h2>HOME</h2>
+				<h2>Front End Course Directory</h2>
 				<p>This fun directory is a project for the <em>React Router Basics</em> course on Treehouse.</p>
 				<p>Learn front end web development and much more! This simple directory app offers a preview of our course
 					library. Choose from many hours of content, from HTML to CSS to JavaScript. Learn to code and get the
 					skills you need to launch a new career in front end web development.</p>
 				<p>We have thousands of videos created by expert teachers on web design and front end development. Our
 					library is continually refreshed with the latest on web technology so you will never fall behind.</p>
-				<hr/>
-
 			</div>
 		);
 	}
@@ -33,12 +32,32 @@ class About extends Component {
 	}
 }
 
-class Repos extends Component {
+class Teachers extends Component {
+	render() {
+		return (
+			<div className="main-content">
+      <h2>Teachers</h2>
+      <ul className="group">
+        {TeacherList.map((teacher) => {
+          return (
+            <li className="teacher" key={teacher.id} >
+              <img className="teacher-img" src={teacher.img_src} alt="teacher" />
+              <h3>{teacher.name}</h3>
+              <p>{teacher.bio}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+		);
+	}
+}
+
+class Courses extends Component {
 	render() {
 		const {route} = this.props;
 		let CurrentList = null;
 		switch (route) {
-
 			case 'css':
 				CurrentList = ['How to Make a CSS', 'HTML CSS'].map( (item, index) => {
 					return <li key = {index}> {item} </li>
@@ -58,11 +77,11 @@ class Repos extends Component {
 		return (
 			<div className="main-content courses">
 				<div className="course-header group">
-					<h2>REPOS</h2>
+					<h2>COURSES</h2>
 					<ul className="course-nav">
-						<li><a href='#/repos/html'>HTML</a></li>
-						<li><a href='#/repos/css'>CSS</a></li>
-						<li><a href='#/repos/javascript'>JavaScript</a></li>
+						<li><a href='#/courses/html'>HTML</a></li>
+						<li><a href='#/courses/css'>CSS</a></li>
+						<li><a href='#/courses/javascript'>JavaScript</a></li>
 					</ul>
 
 					<ul>
@@ -87,7 +106,7 @@ class App extends Component {
 	componentDidMount() {
 		window.addEventListener('hashchange', () => {
 			//<a href="#/about">About</a>
-			//<li><a href='#/repos/html'>HTML</a></li>
+			//<li><a href='#/courses/html'>HTML</a></li>
 			console.log ( window.location.hash.substr(1) );
 
 			this.setState({
@@ -97,45 +116,55 @@ class App extends Component {
 	}
 	render() {
 		let Child;
-		let propsForRepos = null;
+		let propsForCourses = null;
 		switch (this.state.route) {
 			case '/about':
 				Child = About;
+        break;
+      case '/teachers':
+				Child = Teachers;
 				break;
-			case '/repos':
-				Child = Repos;
+			case '/courses':
+				Child = Courses;
 				break;
-			case '/repos/html':
-				Child = Repos;
-				propsForRepos = 'html';
+			case '/courses/html':
+				Child = Courses;
+				propsForCourses = 'html';
 				break;
-			case '/repos/css':
-				Child = Repos;
-				propsForRepos = 'css';
+			case '/courses/css':
+				Child = Courses;
+				propsForCourses = 'css';
 				break;
-			case '/repos/javascript':
-				Child = Repos;
-				propsForRepos = 'javascript';
+			case '/courses/javascript':
+				Child = Courses;
+				propsForCourses = 'javascript';
 				break;
 			default:
 				Child = Home;
 		}
 		return (
-         <div>
-            <header>App</header>{' '}
-            <menu>
-               <ul>
-                  <li>
-                     <a href="#/about">About</a>
-                  </li>{' '}
-                  <li>
-                     <a href="#/repos">Repos</a>
-                  </li>
-               </ul>{' '}
-            </menu>
+         <div data-reactroot className='container'>
+            <header>
+              <span className='icn-logo'><i className='material-icons'>code</i></span>
+              <ul className='main-nav'>
+                <li>
+                  <a href="#/" aria-current='true'>Home</a>
+                </li>
+                <li>
+                  <a href="#/about" aria-current='false'>About</a>
+                </li>
+                <li>
+                  <a href="#/teachers" aria-current='false'>Teachers</a>
+                </li>
+                <li>
+                  <a href="#/courses" aria-current='false'>Courses</a>
+                </li>
+              </ul>
+
+            </header>
 	         {
-	         	propsForRepos?
-			         <Child route = {propsForRepos} />
+	         	propsForCourses?
+			         <Child route = {propsForCourses} />
 		         :
 			         <Child />
 	         }
